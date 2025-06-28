89845c2e87243a6537cdf1c53ffab5179a893010
vinay singhal <vinaysinghal.se@gmail.com>
2025-06-28 11:02:42 +0530

    data Types

--- 01_basics/02_dataTypes.js
+++ 01_basics/02_dataTypes.js
@@ -1,25 +1,22 @@
 "use strict"; // treat all JS code as newer version
 
-// alert( 3 + 3) // we are using nodejs, not browser
-
-console.log(3 
-    +
-     3) // code readability should be high
-
-console.log("Hitesh")
+// alert( 3 + 3) // we are using nodejs, not browser so browser me pop up ke liye use aata hai node js me differnt syntax hai
 
 
-let name = "hitesh"
-let age = 18
-let isLoggedIn = false
-let state;
+let name = "hitesh";      //string data type
+let age = 18;             //number data type
+let weight = 95.67;       //number data type
+let isLoggedIn = false;   //boolean data type
+let state;                
+ 
 
+// ranges of data type
 // number => 2 to power 53
-// bigint
+// bigint  //for  very big number
 // string => ""
 // boolean => true/false
-// null => standalone value
-// undefined => 
+// null => standalone value  
+// undefined => value not defined 
 // symbol => unique
 
 
