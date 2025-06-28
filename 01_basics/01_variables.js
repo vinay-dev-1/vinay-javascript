0e91bc73f61df457a9dcf6bd01406f0deccca9fb
vinay singhal <vinaysinghal.se@gmail.com>
2025-06-28 10:21:17 +0530

    changes in variable

--- 01_basics/01_variables.js
+++ 01_basics/01_variables.js
@@ -1,22 +1,24 @@
 const accountId = 144553
 let accountEmail = "hitesh@google.com"
 var accountPassword = "12345"
-accountCity = "Jaipur"
-let accountState;
+accountCity = "Jaipur"  // not use but aisa ho sakta hai possible hai
+let accountState;   // output will be undefined because koi value define nahi kari
 
-// accountId = 2 // not allowed
+// accountId = 2 // not allowed because of const keyword
 
 
 accountEmail = "hc@hc.com"
 accountPassword = "21212121"
 accountCity = "Bengaluru"
 
-console.log(accountId);
+console.log(accountId);  // not easy method because again and again right console.log so use console.table
+
+//console.table   "issme ek baar me hi sare print karwa do baar baar nahi likhna padhta"
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState])
+
 
 /*
 Prefer not to use var
 because of issue in block scope and functional scope
+faces some debugging isssues 
 */
-
-
-console.table([accountId, accountEmail, accountPassword, accountCity, accountState])
